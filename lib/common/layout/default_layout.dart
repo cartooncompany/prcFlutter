19696bb5acc039bc011ef1f7730f818b269d505a
taejun <ptj01036251@gmail.com>
2024-12-13 23:05:07 +0900

    DefaultLayout 작성

--- lib/common/layout/default_layout.dart
+++ lib/common/layout/default_layout.dart
@@ -11,6 +11,7 @@ class DefaultLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: child,
     );
   }
