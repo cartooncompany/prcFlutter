e3073b91f00a83b35a90e9fad47ed52a9f7e6cbf
taejun <ptj01036251@gmail.com>
2024-12-16 15:05:48 +0900

    Dio로 Auth API 요청해보기

--- lib/uesr/view/login_screen.dart
+++ lib/uesr/view/login_screen.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:untitled/common/const/colors.dart';
 import 'package:untitled/common/layout/default_layout.dart';
@@ -9,6 +13,13 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dio = Dio();
+
+    final emulaterIP = "10.0.2.2:3000";
+    final simulatorIP = "127.0.0.7:3000";
+
+    final ip = Platform.isIOS ? simulatorIP : emulaterIP;
+
     return DefaultLayout(
       child: SingleChildScrollView(
         keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
@@ -21,7 +32,9 @@ class LoginScreen extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 const _Title(),
-                const SizedBox(height: 16.0,),
+                const SizedBox(
+                  height: 16.0,
+                ),
                 const _SubTitle(),
                 Image.asset(
                   'asset/img/misc/logo.png',
@@ -31,15 +44,30 @@ class LoginScreen extends StatelessWidget {
                   hintText: '이메일을 입력해주세요.',
                   onChanged: (String value) {},
                 ),
-                const SizedBox(height: 16.0,),
+                const SizedBox(
+                  height: 16.0,
+                ),
                 CustomTextFormField(
                   hintText: '비밀번호를 입력해주세요.',
                   obscureText: true,
                   onChanged: (String value) {},
                 ),
-                const SizedBox(height: 16.0,),
+                const SizedBox(
+                  height: 16.0,
+                ),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    var rawString = 'test@codefactory.ai:testtest';
+
+                    Codec<String, String> stringToBase64 = utf8.fuse(base64);
+
+                    String token = stringToBase64.encode(rawString);
+
+                    final resp = await dio.post("http://$ip/auth/login",
+                        options: Options(headers: {
+                          'authorization': 'Basic $token',
+                        }));
+                  },
                   style: ElevatedButton.styleFrom(
                     backgroundColor: PRIMARY_COLOR,
                   ),
