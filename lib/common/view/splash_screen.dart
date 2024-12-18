6fee87c1645c94110be0039fd0d1b3b3e40fd2af
taejun <ptj01036251@gmail.com>
2024-12-18 10:59:37 +0900

    splashScreen 구현해보기

--- lib/common/view/splash_screen.dart
+++ lib/common/view/splash_screen.dart
@@ -1,24 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:untitled/common/const/colors.dart';
+import 'package:untitled/common/const/data.dart';
 import 'package:untitled/common/layout/default_layout.dart';
+import 'package:untitled/common/view/root_tab.dart';
+import 'package:untitled/uesr/view/login_screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    checkToken();
+  }
+
+  void checkToken() async {
+    final refreshToken = await storage.read(key: REFRESH_TOKEN_KEY);
+    final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
+
+    if (refreshToken == null || accessToken == null) {
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(
+            builder: (_) => const LoginScreen(),
+          ),
+          (route) => false);
+    } else {
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(
+            builder: (_) => const RootTab(),
+          ),
+          (route) => false);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
       backgroundColor: PRIMARY_COLOR,
       child: SizedBox(
-        width: MediaQuery.of(context).size.width/2,
+        width: MediaQuery.of(context).size.width,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset('asset/img/logo/logo.png', width: MediaQuery.of(context).size.width/2,),
-            const SizedBox(
-              height: 16.0,
+            Image.asset(
+              'asset/img/logo/logo.png',
+              width: MediaQuery.of(context).size.width / 2,
             ),
-            const CircularProgressIndicator(color: Colors.white,),
+            const SizedBox(height: 16.0),
+            CircularProgressIndicator(
+              color: Colors.white,
+            ),
           ],
         ),
       ),
