97af118f3617724a6674506e5b0998f487c64d3b
taejun <ptj01036251@gmail.com>
2024-12-19 23:25:19 +0900

    feat: ProductCard 컴포넌트 구현 및 RestaurantDetailScreen 적용
    RestaurantDetailScreen에 ProductCard를 추가하여 메뉴
    정보를 표시합니다.
    
    - ProductCard 컴포넌트를 새로 생성하여 디자인 및 데이터 표시
    - RestaurantDetailScreen에 ProductCard를 추가하여
      레스토랑 상세 정보와 함께 메뉴 정보를 보여주도록 개선

--- lib/restaurant/view/restaurant_detail_screen.dart
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:untitled/common/layout/default_layout.dart';
+import 'package:untitled/product/component/product_card.dart';
 import 'package:untitled/restaurant/component/restaurant_card.dart';
 
 class RestaurantDetailScreen extends StatelessWidget {
@@ -8,23 +9,26 @@ class RestaurantDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
-        title: '불타는 떡볶이',
-        child: Column(
-          children: [
-            RestaurantCard(
-              img: Image.asset(
-                'asset/img/food/ddeok_bok_gi.jpg'
-              ),
-              name: '불타는 떡볶이',
-              tags: const ['떡볶이', '맛있음', '치즈'],
-              ratingsCount: 100,
-              deliveryTime: 30,
-              deliveryFee: 3000,
-              ratings: 4.76,
-              isDetail: true,
-              detail: '맛있는 떡볶이',
-            ),
-          ],
-        ));
+      title: '불타는 떡볶이',
+      child: Column(
+        children: [
+          RestaurantCard(
+            img: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
+            name: '불타는 떡볶이',
+            tags: const ['떡볶이', '맛있음', '치즈'],
+            ratingsCount: 100,
+            deliveryTime: 30,
+            deliveryFee: 3000,
+            ratings: 4.76,
+            isDetail: true,
+            detail: '맛있는 떡볶이',
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: ProductCard(),
+          ),
+        ],
+      ),
+    );
   }
 }
