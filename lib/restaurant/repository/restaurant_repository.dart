83e606f3fe66a09a8b73095e02f69b1686f32429
taejun <ptj01036251@gmail.com>
2025-02-17 11:33:30 +0900

    pagination UI에 적용해보기

--- lib/restaurant/repository/restaurant_repository.dart
+++ lib/restaurant/repository/restaurant_repository.dart
@@ -1,5 +1,7 @@
 import 'package:dio/dio.dart' hide Headers;
 import 'package:retrofit/retrofit.dart';
+import 'package:untitled/common/model/cursor_pagination_model.dart';
+import 'package:untitled/restaurant/model/restaurant_model.dart';
 
 import '../model/restaurant_detail_model.dart';
 
@@ -7,9 +9,18 @@ part 'restaurant_repository.g.dart';
 
 @RestApi()
 abstract class RestaurantRepository {
+  // http://$ip/restaurant
   factory RestaurantRepository(Dio dio, {String baseUrl}) =
-      _RestaurantRepository;
+  _RestaurantRepository;
 
+  // http://$ip/restaurant/
+  @GET('/')
+  @Headers({
+    'accessToken': 'true',
+  })
+  Future<CursorPagination<RestaurantModel>> paginate();
+
+  // http://$ip/restaurant/:id
   @GET('/{id}')
   @Headers({
     'accessToken': 'true',
--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -1,7 +1,9 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:untitled/common/dio/dio.dart';
 import 'package:untitled/restaurant/component/restaurant_card.dart';
 import 'package:untitled/restaurant/model/restaurant_model.dart';
+import 'package:untitled/restaurant/repository/restaurant_repository.dart';
 import 'package:untitled/restaurant/view/restaurant_detail_screen.dart';
 
 import '../../common/const/data.dart';
@@ -9,21 +11,16 @@ import '../../common/const/data.dart';
 class RestaurantScreen extends StatelessWidget {
   const RestaurantScreen({super.key});
 
-  Future<List> paginateRestaurant() async {
+  Future<List<RestaurantModel>> paginateRestaurant() async {
     final dio = Dio();
 
-    final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
-
-    final resp = await dio.get(
-      'http://$ip/restaurant',
-      options: Options(
-        headers: {
-          'authorization': 'Bearer $accessToken',
-        },
-      ),
+    dio.interceptors.add(
+      CustomInterceptor(storage: storage),
     );
 
-    return resp.data['data'];
+    final resp = await RestaurantRepository(dio, baseUrl: "http://$ip/restaurant").paginate();
+
+    return resp.data;
   }
 
   @override
@@ -31,9 +28,9 @@ class RestaurantScreen extends StatelessWidget {
     return Center(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: FutureBuilder<List>(
+        child: FutureBuilder<List<RestaurantModel>>(
           future: paginateRestaurant(),
-          builder: (context, AsyncSnapshot<List> snapshot) {
+          builder: (context, AsyncSnapshot<List<RestaurantModel>> snapshot) {
             if (!snapshot.hasData) {
               return const Center(
                 child: CircularProgressIndicator(),
@@ -42,8 +39,7 @@ class RestaurantScreen extends StatelessWidget {
               return ListView.separated(
                 itemCount: snapshot.data!.length,
                 itemBuilder: (_, index) {
-                  final item = snapshot.data![index];
-                  final pItem = RestaurantModel.fromJson(item);
+                  final pItem = snapshot.data![index];
 
                   return GestureDetector(
                       onTap: () {
